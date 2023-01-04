804f31cffa1fbecaf2b8530e294446c3b9c2550b
Oscar Zambotti <oscar.zambotti@factorymind.com>
2023-01-04 14:03:58 +0100

    Enable server-side rendering (SSR)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import netlify from "@astrojs/netlify/functions";
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://www.ilcucchiaiononesiste.it',
-	integrations: [mdx(), sitemap()],
+  site: 'https://www.ilcucchiaiononesiste.it',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
