4eeac0536742a3985f4b6075b54693968a672999
Oscar Zambotti <oscar.zambotti@factorymind.com>
2023-01-04 14:09:53 +0100

    Rollback to static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import netlify from "@astrojs/netlify/functions";
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.ilcucchiaiononesiste.it',
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: netlify()
+  integrations: [mdx(), sitemap()]
 });
