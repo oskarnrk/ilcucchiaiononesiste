5085c6175ff67e50ce8eb71214a8775e83062675
Oscar Zambotti <oscar.zambotti@factorymind.com>
2024-04-16 00:04:03 +0200

    Astro updated, GA commented, style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ilcucchiaiononesiste.it",
+  i18n: {
+    defaultLocale: "it-it",
+    locales: ["it-it"],
+  },
   integrations: [
     mdx(),
     partytown({
